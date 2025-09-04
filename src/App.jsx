124c36abb81e16c748a9606792a40dd28e641f2e
Andre Jarl <andrejarlaniana@gmail.com>
2025-09-04 19:47:24 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <>
+      <div>
+     
+      </div>
+      <p className="text-red-500 text-center mt-10 pt-1">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
