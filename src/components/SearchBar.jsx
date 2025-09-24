fd48ac1f0bd12a83b691725a945bbf003e848245
Andre Jarl <andrejarlaniana@gmail.com>
2025-09-24 18:54:24 +0800

    remove the department logos below the search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -15,7 +15,7 @@ function SearchBar({searchTerm, suggestions, handleSearch, handleSuggestionClick
 
   return (
      <div className="fixed top-3  group right lg:right-10 2xl:right-36 2xl:top-6 px-2" >
-            <div className=" rounded-t-lg border-x border-t border-gray-200  w-[300px] lg:w-[400px] 2xl:w-[600px]   px-5 flex flex-row justify-center items-center bg-white hover:rounded-t-xl">
+            <div className=" rounded-lg border-x border border-gray-200  w-[300px] lg:w-[400px] 2xl:w-[600px]   px-5 flex flex-row justify-center items-center bg-white hover:rounded-t-xl">
             <input onChange={handleSearch} className="px-2 py-3 bg-white w-[200px] lg:w-[400px] 2xl:w-[600px]  border-none outline-none text-sm overflow-hidden" type="search" name="" id="" placeholder="Search building/facilities"
            
             />
@@ -26,7 +26,7 @@ function SearchBar({searchTerm, suggestions, handleSearch, handleSuggestionClick
            
             </div>
         
-              <div className=" rounded-b-lg shadow-xl  w-[300px] lg:w-[400px] 2xl:w-[600px] border-x border-gray-200  px-5 flex flex-row justify-center items-center bg-white">
+              {/* <div className=" rounded-b-lg shadow-xl  w-[300px] lg:w-[400px] 2xl:w-[600px] border-x border-gray-200  px-5 flex flex-row justify-center items-center bg-white">
                <div className="grid grid-cols-4 justify-center mt-4 gap-5 items-center pb-4">
                   <div className="flex flex-col gap-2 items-center">
                   <img className="w-10" src={eng}  alt="" srcset="" />
@@ -46,7 +46,7 @@ function SearchBar({searchTerm, suggestions, handleSearch, handleSuggestionClick
                   </div>
                </div>
 
-            </div>
+            </div> */}
   
                    
         
@@ -59,7 +59,7 @@ function SearchBar({searchTerm, suggestions, handleSearch, handleSuggestionClick
                   <i className="text-sm text-slate-400">No building/facilities found.</i>
                </div> 
         ): (
-            <div className={`bg-white rounded-b-lg shadow-xl shadow-gray-400 max-h-[460px] overflow-auto flex-col gap-1 pt-2 py-2 items-start justify-start -mt-2 ${suggestions.length>0 ? 'flex' : 'hidden'}`
+            <div className={`bg-white rounded-b-lg shadow-md shadow-gray-500 max-h-[460px] overflow-auto flex-col gap-1 pt-2 py-2 items-start justify-start -mt-2 ${suggestions.length>0 ? 'flex' : 'hidden'}`
                 }>
                  
                  
