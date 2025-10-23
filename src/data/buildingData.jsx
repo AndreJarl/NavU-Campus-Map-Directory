059a56b1420b8336121a7527a6194228ea3fab89
Andre Jarl <andrejarlaniana@gmail.com>
2025-10-23 21:33:40 +0800

    add onlick on new admin rooms and engineering rooms

--- src/data/buildingData.jsx
+++ src/data/buildingData.jsx
@@ -34,7 +34,7 @@ const buildingData = {
       { 
         
         code: " ", 
-        name: " Electronic Laboratory",
+        name: "Electronic Laboratory",
         description: " A well-equipped room with computers and internet access used for programming, research, and computer-based lessons.",
         img: " "
 
@@ -438,7 +438,7 @@ img:" "
               {
               
               code: " ",
-              name: "Computer Laboratory",
+              name: "COT Computer Laboratory",
               description: " room equipped with computers and internet access where students learn programming, digital applications, and other computer-related skills. ",
               img: " "
               }, 
@@ -674,43 +674,43 @@ img:" "
    1:[
 {
 code:"",
-name:"Accounting Office ",
+name:"Accounting Office",
 description: "The office responsible for managing the school’s financial transactions, including payments, fees, and other accounting records. ",
 img: ""
 },
 {
 code:"",
-name:"MIS Office ",
+name:"MIS Office",
 description: "The office that handles the school’s data management, computer systems, and network services. ",
 img: ""
 },
 {
 code:"",
-name:"Registrar ",
+name:"Registrar",
 description: "The office that manages student records, enrollment, grades, and official school documents. ",
 img: ""
 }, 
 {
 code:"",
-name:"Cashier ",
+name:"Cashier",
 description: "The office where students and staff process payments, fees, and other financial transactions. ",
 img: ""
 },
 {
 code:"",
-name:"BAC Office ",
+name:"BAC Office",
 description: "The office where procurement processes, bidding, and acquisition of school goods and services are managed. ",
 img: ""
 },
 {
 code:"",
-name:"University Clinic ",
+name:"University Clinic",
 description: "A healthcare facility on campus where students and staff receive medical attention, consultations, and basic health services. ",
 img: ""
 }, 
 {
 code:"",
-name:"Power House ",
+name:"Power House",
 description: "A facility that supplies and manages electricity for the school, ensuring a stable and continuous power supply for all campus operations. ",
 img: ""
 },
